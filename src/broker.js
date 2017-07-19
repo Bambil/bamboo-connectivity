f414632b7944139616c3ee5c042c4565fad77320
Parham Alvani <parham.alvani@gmail.com>
2017-07-20 00:26:48 +0430

    Creates I1820 broker class.

--- src/broker.js
+++ src/broker.js
@@ -7,77 +7,71 @@
  * | File Name:     broker.js
  * +===============================================
  */
-const mosca = require('mosca')
 const winston = require('winston')
+const mosca = require('mosca')
 
 const agent = require('./agent')
 const Message = require('./message')
 
-const pubsubSettings = {
-  type: 'mongo',
-  url: `mongodb://${process.env.I1820_MONGO_URL}/mqtt`,
-  pubsubCollection: 'ascoltatori',
-  mongo: {}
-}
+class I1820Broker extends mosca.Server {
+  constructor (options) {
+    super(options)
 
-const moscaSettings = {
-  port: 1883,
-  backend: pubsubSettings
-}
+    this.on('clientConnected', (client) => {
+      let result = client.id.match(/^I1820\/(\w+)\/agent\/(\w+)/i)
+      if (result && result.length === 3) {
+        let tenant = result[1]
+        let name = result[2]
+        agent.createAgent(name, tenant).then((a) => {
+          winston.info(' Adding new agent:')
+          a.toString().split('\n').forEach((l) => {
+            winston.data(l)
+          })
 
-const server = new mosca.Server(moscaSettings)
+          this.publish({
+            topic: `I1820/${a.tenant}/agent/${a.name}`,
+            payload: a.hash,
+            qos: 0,
+            retain: false
+          }, (err, p) => {
+            if (err) {
+              winston.error(err)
+              return
+            }
+            winston.info(' ID to agent:')
+            winston.data(p)
+          })
+        })
+      }
+    })
 
-// fired when the mqtt server is ready
-server.on('ready', function () {
-  winston.info(` * MQTT at 0.0.0.0:${moscaSettings.port}`)
-})
-
-// fired when a client connects
-server.on('clientConnected', function (client) {
-  let result = client.id.match(/^I1820\/(\w+)\/agent\/(\w+)/i)
-  if (result && result.length === 3) {
-    let tenant = result[1]
-    let name = result[2]
-    agent.createAgent(name, tenant).then((a) => {
-      winston.data(' Agent:')
-      a.toString().split('\n').forEach((l) => {
-        winston.data(l)
-      })
-
-      server.publish({
-        topic: `I1820/${a.tenant}/agent/${a.name}`,
-        payload: a.hash,
-        qos: 0,
-        retain: false
-      }, (err) => {
-        if (err) {
-          winston.error(err)
+    this.on('published', (packet, client, callback) => {
+      if (client) {
+        let result = packet.topic.match(/^I1820\/(\w+)\/agent\/(\w+)/i)
+        if (result && result.length === 3) {
+          let tenant = result[1]
+          let action = result[2]
+          if (action === 'ping') {
+            winston.info(packet.payload.toString())
+            let m = Message.fromJSON(packet.payload)
+            if (m) {
+              agent.pingAgent(m.name, tenant, m.hash)
+              winston.info(` ping from ${m.name} @ ${tenant}`)
+            }
+          } else if (action === 'log') {
+            let m = Message.fromJSON(packet.payload)
+            if (m && agent.validateAgent(m.name, tenant, m.hash)) {
+            }
+          }
         }
-      })
+      }
     })
   }
 
-  result = client.id.match()
-})
-
-// fired when a message is received
-server.on('published', function (packet, client) {
-  if (client) {
-    let result = packet.topic.match(/^I1820\/(\w+)\/agent\/(\w+)/i)
-    if (result && result.length === 3) {
-      let tenant = result[1]
-      let action = result[2]
-      if (action === 'ping') {
-        let m = Message.fromJSON(packet.payload)
-        if (m) {
-          agent.pingAgent(m.name, tenant, m.hash)
-          winston.info(` ping from ${m.name} @ ${tenant}`)
-        }
-      } else if (action === 'log') {
-        let m = Message.fromJSON(packet.payload)
-        if (m && agent.validateAgent(m.name, tenant, m.hash)) {
-        }
-      }
-    }
+  authenticate (client, username, password, callback) {
+    callback(null, true)
   }
-})
+}
+
+// fired when a client connects
+module.exports = I1820Broker
