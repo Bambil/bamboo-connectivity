7db2017950c32296f7fc7b93f164b4b0b6fa602a
Parham Alvani <parham.alvani@gmail.com>
2017-07-19 18:41:39 +0430

    Typo was corrected.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
 
-mongoose.connect(`mongodb://${process.env.mongo_url}/I1820`, {
+mongoose.connect(`mongodb://${process.env.I1820_MONOG_URL}/I1820`, {
   useMongoClient: true
 }).then(() => {
   winston.info(' * DB connection was created')
