0c755a016d726a5437e03ef81e6fefd9b7eb8ea9
Parham Alvani <parham.alvani@gmail.com>
2017-07-20 15:17:56 +0430

    Adds I1820 hello message.

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ const winston = require('winston')
 
 /* Configure CLI output on the default logger */
 winston.cli()
+winston.info(' * 18.20 at Sep 07 2016 7:20 IR721')
 
 /* Mongoose initiation */
 const mongoose = require('mongoose')
