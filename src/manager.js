65437f2e526429b047e65608c3c1b52d93c49cf2
Parham Alvani <parham.alvani@gmail.com>
2017-07-21 16:09:00 +0430

    I1820 manager is a manager for I1820 components.

---
+++ src/manager.js
@@ -0,0 +1,30 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani (parham.alvani@gmail.com)
+ * |
+ * | Creation Date: 21-07-2017
+ * |
+ * | File Name:     manager.js
+ * +===============================================
+ */
+
+class I1820Manager {
+  constructor () {
+    this.channels = {
+      'I1820/log': {}
+    }
+  }
+
+  addComponent (name, id, channel) {
+    if (channel in this.channels) {
+      if (name in this.channels[channel]) {
+        this.channels[channel][name].add(id)
+      } else {
+        this.channels[channel][name] = new Set([id])
+      }
+      console.log(this.channels)
+    }
+  }
+}
+
+module.exports = I1820Manager
