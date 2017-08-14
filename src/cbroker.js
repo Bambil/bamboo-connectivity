12f47bceb7263756ad64fffa40d814f5dd375762
Parham Alvani <parham.alvani@gmail.com>
2017-08-14 19:37:14 +0430

    Cluster version of broker was improved.

--- src/cbroker.js
+++ src/cbroker.js
@@ -1,26 +1,45 @@
 const aedes = require('aedes')
+const net = require('net')
 const cluster = require('cluster')
+const winston = require('winston')
+const EventEmitter = require('events')
 
-class BambooBroker {
-  constructor (processNumber) {
+if (cluster.isWorker) {
+  const server = net.createServer(aedes.handle)
+
+  server.listen(process.env.port, () => {
+  })
+}
+
+class BambooBroker extends EventEmitter {
+  constructor (port, processNumber) {
+    super()
+    this.port = port
     this.processNumber = processNumber
+
+    cluster.setupMaster({
+      exec: './src/cbroker'
+    })
   }
 
   run () {
-    if (cluster.isMaster) {
-      console.log(`Master ${process.pid} is running`)
+    winston.info(` * Master ${process.pid} is running`)
 
-      // Fork workers.
-      for (let i = 0; i < numCPUs; i++) {
-        cluster.fork()
-      }
-
-      cluster.on('exit', (worker, code, signal) => {
-        console.log(`worker ${worker.process.pid} died`)
+    // Fork workers.
+    for (let i = 0; i < this.processNumber; i++) {
+      cluster.fork({
+        port: this.port
       })
-
-    } else {
     }
+
+    cluster.on('exit', (worker, code, signal) => {
+      winston.error(`worker ${worker.process.pid} died`)
+    })
+
+    cluster.on('listening', (worker, address) => {
+      winston.info(` * Worker ${worker.id} on ${address.port}`)
+      this.emit('ready')
+    })
   }
 }
 
