cdf14b5d7944833be033164778ea8034b5301c89
Parham Alvani <parham.alvani@gmail.com>
2017-08-25 23:53:05 +0430

    Adds winstond configurations.

--- config/custom-environment-variables.js
+++ config/custom-environment-variables.js
@@ -2,5 +2,9 @@ module.exports = {
   broker: {
     port: 'BAMBOO_BROKER_PORT',
     processes: 'BAMBOO_BROKER_PROCESSES'
+  },
+  winstond: {
+    host: 'BAMBOO_WINSTOND_HOST',
+    port: 'BAMBOO_WINSTOND_PORT'
   }
 }
--- config/default.js
+++ config/default.js
@@ -2,5 +2,9 @@ module.exports = {
   broker: {
     port: 1883,
     processes: 2
+  },
+  winstond: {
+    host: '127.0.0.1',
+    port: 9003
   }
 }
