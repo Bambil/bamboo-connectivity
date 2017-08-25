56e789e489c27db2a825b9afe396cd6f0d0dd73b
Parham Alvani <parham.alvani@gmail.com>
2017-08-25 23:47:20 +0430

    Adds bamboo logger.

---
+++ src/logger.js
@@ -0,0 +1,39 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 24-08-2017
+ * |
+ * | File Name:     src/logger.js
+ * +===============================================
+ */
+const winston = require('winston')
+const config = require('config')
+
+const logger = new winston.Logger({
+  rewriters: [function (level, msg, meta) {
+    if (meta) {
+      meta.component = 'bamboo-log'
+    } else {
+      meta = {
+        component: 'bamboo-log'
+      }
+    }
+    return meta
+  }],
+
+  transports: [new winston.transports.File({
+    filename: 'bamboo-log.log',
+    colorize: true,
+    timestamp: true,
+    prettyPrint: true,
+    json: false,
+    tailable: true,
+    maxFiles: 1
+  }), new winston.transports.Http({
+    host: config.winstond.host,
+    port: config.winstond.port
+  })]
+})
+
+module.exports = logger
