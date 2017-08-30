e31d9eb46c838c9633148e235c14ca4882e1224d
Parham Alvani <parham.alvani@gmail.com>
2017-08-31 01:58:54 +0430

    Provides worker as entry point for all workers.

---
+++ src/worker.js
@@ -0,0 +1,15 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 31-08-2017
+ * |
+ * | File Name:     src/worker.js
+ * +===============================================
+ */
+const BambooBrokerWorker = require('./broker')
+const cluster = require('cluster')
+
+if (cluster.isWorker) {
+  new BambooBrokerWorker().run()
+}
