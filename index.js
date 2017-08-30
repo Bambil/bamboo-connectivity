2ad6f9e2144450058fc37340faf9457f0a4d05a4
Parham Alvani <parham.alvani@gmail.com>
2017-08-31 01:58:12 +0430

    Uses bamboo master in index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const chalk = require('chalk')
 vorpal
   .command('fork', 'forks new broker process')
   .action(function (args, callback) {
-    bambooBroker.fork()
+    bambooMaster.fork()
     callback()
   })
 
@@ -27,7 +27,7 @@ vorpal
     this.log(` * Master - ${process.pid}`)
     this.log(` # CPU Usage: ${process.cpuUsage().user} millionth of a second`)
     this.log(` # Memory Usage: ${process.memoryUsage().heapUsed} bytes`)
-    for (let worker of bambooBroker.workers) {
+    for (let worker of bambooMaster.workers) {
       this.log(` * Worker ${worker.id} - ${worker.process.pid}`)
     }
     callback()
@@ -36,11 +36,11 @@ vorpal
 vorpal
   .command('components', 'lists avaiable components')
   .action(function (args, callback) {
-    for (let channel in bambooBroker.components.channels) {
+    for (let channel in bambooMaster.components.channels) {
       this.log(`${chalk.rgb(255, 255, 186)(channel)}:`)
-      for (let name in bambooBroker.components.channels[channel]) {
+      for (let name in bambooMaster.components.channels[channel]) {
         this.log(`    ${chalk.rgb(186, 225, 255)(name)}:`)
-        for (let id of bambooBroker.components.channels[channel][name]) {
+        for (let id of bambooMaster.components.channels[channel][name]) {
           this.log(`        * component ${chalk.rgb(186, 255, 201)(id)}`)
         }
       }
@@ -52,12 +52,12 @@ vorpal.log(' * 18.20 at Sep 07 2016 7:20 IR721')
 vorpal.delimiter(`${chalk.green('Bamboo')} - ${chalk.rgb(255, 177, 79)('Connectivity')} > `).show()
 
 /* Broker Cluster */
-const BambooBroker = require('./src/cbroker')
+const BambooMaster = require('./src/master')
 
-const bambooBroker = new BambooBroker(
+const bambooMaster = new BambooMaster(
   config.broker.port,
   config.broker.processes
 )
-bambooBroker.on('ready', (worker) => {
+bambooMaster.on('ready', (worker) => {
   vorpal.log(` * MQTT at 0.0.0.0:${config.broker.port} on ${worker.id}`)
 }).run()
