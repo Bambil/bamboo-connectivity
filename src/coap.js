8e8260eed17308d86a7a05c38cf251869b21f3ab
Parham Alvani <parham.alvani@gmail.com>
2017-08-30 09:33:08 +0430

    Adds coap worker basis.

---
+++ src/coap.js
@@ -0,0 +1,23 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 30-08-2017
+ * |
+ * | File Name:     src/coap.js
+ * +===============================================
+ */
+const coap = require('coap')
+const cluster = require('cluster')
+
+class BambooCoAPWorker {
+  run () {
+    const server = coap.createServer()
+    server.listen(process.env.port)
+
+    server.on('request', (req, res) => {
+    })
+  }
+}
+
+module.exports = BambooCoAPWorker
